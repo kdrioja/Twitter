7a4aecc2bf28d9da85472ec1ed22e9f55903d8ed
Kenia Rioja <kdrioja@gmail.com>
2018-10-12 18:10:41 -0700

    Retweet and favorite buttons debugged

--- twitter_alamofire_demo/APIManager.swift
+++ twitter_alamofire_demo/APIManager.swift
@@ -76,6 +76,7 @@ class APIManager: SessionManager {
 
         // This uses tweets from disk to avoid hitting rate limit. Comment out if you want fresh
         // tweets,
+        /*
         if let data = UserDefaults.standard.object(forKey: "hometimeline_tweets") as? Data {
             let tweetDictionaries = NSKeyedUnarchiver.unarchiveObject(with: data) as! [[String: Any]]
             let tweets = tweetDictionaries.flatMap({ (dictionary) -> Tweet in
@@ -85,7 +86,7 @@ class APIManager: SessionManager {
             completion(tweets, nil)
             return
         }
-
+        */
         request(URL(string: "https://api.twitter.com/1.1/statuses/home_timeline.json")!, method: .get)
             .validate()
             .responseJSON { (response) in
